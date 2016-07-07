b6725fea5e145b2c56e5afd90cd54f9c3896d3ea
ctarda <ctarda@me.com>
2016-07-07 18:20:03 +0200

    make API public

--- Source/NSURL+URLPreview.swift
+++ Source/NSURL+URLPreview.swift
@@ -15,7 +15,7 @@ extension NSURL {
         static var queue = NSOperationQueue()
     }
     
-    func fetchPageInfo(completion: ((title: String?, description: String?, previewImage: String?) -> Void), failure: ((errorMessage: String) -> Void)) {
+    public func fetchPageInfo(completion: ((title: String?, description: String?, previewImage: String?) -> Void), failure: ((errorMessage: String) -> Void)) {
         
         let request = NSMutableURLRequest(URL: self)
         let newUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
